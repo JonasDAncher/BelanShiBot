fb79f628fc595813ec1e864d094e8f2adfcb7c72
JonasDAncher <JonasDAncher@hotmail.com>
2025-02-20 21:38:55 +0100

    guilds intent

--- BelanShiBot.py
+++ BelanShiBot.py
@@ -16,6 +16,7 @@ DELETE_TIME: Final[int] = 10  # How long after sending an ephemeral message is d
 intents = Intents.default()
 intents.message_content = True  # Allow the bot to see message content
 intents.members = True  # Allow the bot to see message content
+intents.guilds = True
 client = Client(intents=intents, allowed_mentions = discord.AllowedMentions(roles=True, users=True))
 tree = app_commands.CommandTree(client)
 
