8086fe2ddb04b1cf424802a9be8b44038a1a098e
JonasDAncher <JonasDAncher@hotmail.com>
2025-02-22 13:09:51 +0100

    perhaps more

--- BelanShiBot.py
+++ BelanShiBot.py
@@ -516,10 +516,10 @@ If you are __not__ a server admin, you should inform one of the issue.""")
 # @tree.command()                                 # Adds command globally
 async def roles(interaction: discord.Interaction):
   """Creates a message with buttons to self-assign roles"""
-  content_var = """**You can self-assign roles using the buttons below**\n
-  Click the buttons corrosponding to the role, you wish to receive pings for, when a `/key` run is started!\n
-  Press any role you __already__ have, to disable pings again.\n\n
-  *This message disappears in 2 minutes*"""  
+  content_var = """**You can self-assign roles using the buttons below**
+  Click the buttons corrosponding to the role, you wish to receive pings for, when a `/key` run is started!
+  Press any role you __already__ have, to disable pings again.\n
+  *`This message disappears in 2 minutes`*"""  
   await interaction.response.send_message(content=content_var, 
                                           view=AssignRoles(interaction), 
                                           ephemeral=True, 
