83f76228d73b2b568d5714c7d3fcf8ac93c2adbd
JonasDAncher <JonasDAncher@hotmail.com>
2025-02-28 16:47:14 +0100

    maybe remember to subtract??

--- BelanShiBot.py
+++ BelanShiBot.py
@@ -601,6 +601,7 @@ class Quiz(ui.View):
       @tasks.loop(seconds=1.0, count=5)
       async def timer(self):
         embed_dict = interaction.message.embeds[0].to_dict()
+        self.timer_time -= 1
         print(self.timer_time)
         for field in embed_dict["fields"]:
           if field["name"] == "TIMER": field["value"] = f"{self.timer_time} seconds left to answer!"
