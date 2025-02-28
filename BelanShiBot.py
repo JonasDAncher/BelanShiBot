84951cbb0f12b39990956947468a2b5d00c542e6
JonasDAncher <JonasDAncher@hotmail.com>
2025-02-28 16:18:51 +0100

    seconds=

--- BelanShiBot.py
+++ BelanShiBot.py
@@ -598,7 +598,7 @@ class Quiz(ui.View):
       embed_var.add_field(name="4", value=f"> {options[question_number][3]}", inline=True)
       await interaction.message.edit(embed=embed_var)
 
-      @tasks.loop(1.0, count=5)
+      @tasks.loop(seconds=1.0, count=5)
       async def timer(timer_time):
         timer_time -= 1
         embed_dict = interaction.message.embeds[0].to_dict()
