19db22577324110d1066943c8664d2e53dea97ca
JonasDAncher <JonasDAncher@hotmail.com>
2025-02-28 14:04:05 +0100

    start button updates view

--- BelanShiBot.py
+++ BelanShiBot.py
@@ -587,6 +587,7 @@ class Quiz(ui.View):
         self.add_item(b_button)
         self.add_item(c_button)
         self.add_item(d_button)
+        await interaction.message.edit(view=self)
         await interaction.response.send_message(content=f"You've started the quiz.\n-# *This message disappears in {5} seconds*", ephemeral=True, delete_after=5)
     
     start_button.callback = start
