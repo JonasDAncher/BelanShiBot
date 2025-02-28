ea78855b4702907c2d045ac333ff0cc63a584282
JonasDAncher <JonasDAncher@hotmail.com>
2025-03-01 00:44:57 +0100

    eeeh

--- BelanShiBot.py
+++ BelanShiBot.py
@@ -584,19 +584,19 @@ class Quiz(ui.View):
       # --------- Manipulating the embed ---------
       
       # The int tracking which question the quiz is on.
-      question_number = 1
+      self.question_number = 0
       self.timer_time = 5
       print(interaction.message.embeds)
       print(interaction.message.embeds[0])
       embed_var = interaction.message.embeds[0]
       embed_var.add_field(name="",value="``` ```") # Spacer
-      embed_var.add_field(name=f"Question #*{question_number+1}*",
-                          value=f"**{questions[question_number]}**", inline=False)
-      embed_var.add_field(name="1", value=f"> *{options[question_number][0]}*", inline=True)
-      embed_var.add_field(name="2", value=f"> *{options[question_number][1]}*", inline=True)
+      embed_var.add_field(name=f"Question #*{self.question_number+1}*",
+                          value=f"**{questions[self.question_number]}**", inline=False)
+      embed_var.add_field(name="1", value=f"> *{options[self.question_number][0]}*", inline=True)
+      embed_var.add_field(name="2", value=f"> *{options[self.question_number][1]}*", inline=True)
       embed_var.add_field(name="",value="",inline=False) # New line
-      embed_var.add_field(name="3", value=f"> *{options[question_number][2]}*", inline=True)
-      embed_var.add_field(name="4", value=f"> *{options[question_number][3]}*", inline=True)
+      embed_var.add_field(name="3", value=f"> *{options[self.question_number][2]}*", inline=True)
+      embed_var.add_field(name="4", value=f"> *{options[self.question_number][3]}*", inline=True)
       embed_var.add_field(name="TIMER", value=f"{self.timer_time} seconds left to answer!", inline=False)
       await interaction.message.edit(embed=embed_var)
 
@@ -615,16 +615,16 @@ class Quiz(ui.View):
         for button in self.children:
           if type(button) == ui.Button: button.disabled= True
         await interaction.message.edit(view=self)
-        next_question(self)
+        await next_question(self)
 
       async def next_question(self):
         embed_dict = interaction.message.embeds[0].to_dict()
         for field in embed_dict["fields"]:
-          if field["name"] == f"Question #*{question_number+1}*": field["value"] = f"Question #*{question_number+1}*"
-          if field["name"] == "1": field["value"] = f"> *{options[question_number][0]}*"
-          if field["name"] == "2": field["value"] = f"> *{options[question_number][1]}*"
-          if field["name"] == "3": field["value"] = f"> *{options[question_number][2]}*"
-          if field["name"] == "4": field["value"] = f"> *{options[question_number][3]}*"
+          if field["name"] == f"Question #*{self.question_number+1}*": field["value"] = f"Question #*{self.question_number+1}*"
+          if field["name"] == "1": field["value"] = f"> *{options[self.question_number][0]}*"
+          if field["name"] == "2": field["value"] = f"> *{options[self.question_number][1]}*"
+          if field["name"] == "3": field["value"] = f"> *{options[self.question_number][2]}*"
+          if field["name"] == "4": field["value"] = f"> *{options[self.question_number][3]}*"
         self.timer_time = 5
         await asyncio.sleep(1)
         await timer.start(self)
