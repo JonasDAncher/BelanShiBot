5c4facb47d4b6e4cbe7fa9a9766021a7c88ee951
JonasDAncher <JonasDAncher@hotmail.com>
2025-03-01 01:20:15 +0100

    dammit

--- BelanShiBot.py
+++ BelanShiBot.py
@@ -619,7 +619,7 @@ class Quiz(ui.View):
         print(self.question_number)
         print(len(questions)-1)
         print(self.question_number < len(questions)-1)
-        if self.question_number <= len(questions)-1:
+        if self.question_number < len(questions)-1:
           await next_question(self)
           print("next question...")
         else:
