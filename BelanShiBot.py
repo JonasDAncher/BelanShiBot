f5cbd1146d32dab0628bcf3b2602dd278deeb911
JonasDAncher <JonasDAncher@hotmail.com>
2025-03-01 15:01:02 +0100

    qn is wrong somehow

--- BelanShiBot.py
+++ BelanShiBot.py
@@ -620,8 +620,8 @@ class Quiz(ui.View):
         print(len(questions))
         print(self.question_number <= len(questions))
         if self.question_number <= len(questions):
-          await next_question(self)
           print("next question...")
+          await next_question(self)
         else:
           print("ending quiz...")
           await finish_quiz()
@@ -664,7 +664,9 @@ class Quiz(ui.View):
       async def next_question(self):
         embed_dict = interaction.message.embeds[0].to_dict()
         for field in embed_dict["fields"]:
-          if field["name"] == f"Question #*{self.question_number-1}*": field["name"] = f"Question #*{self.question_number}*"
+          if field["name"] == f"Question #*{self.question_number}*": 
+            field["name"] = f"Question #*{self.question_number+1}*"
+            field["value"] = f"Question #*{self.question_number+1}*"
           if field["name"] == "1": field["value"] = f"> *{options[self.question_number][0]}*"
           if field["name"] == "2": field["value"] = f"> *{options[self.question_number][1]}*"
           if field["name"] == "3": field["value"] = f"> *{options[self.question_number][2]}*"
